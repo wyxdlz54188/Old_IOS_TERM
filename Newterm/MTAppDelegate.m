d82d66a0c5ce03bce326dc1826e8b5ae8ce229d1
wyxdlz54188 <monkeycode-ai@chaitin.com>
2026-06-19 08:23:38 +0000

    feat: TabBar 图标 + 设置页面 Grouped TableView 现代化
    - 程序化绘制 Term 图标(深色底绿色>_符号)和设置图标(齿轮图案)
    - 设置页改为 UITableViewStyleGrouped 分组样式
    - 滑杆放在单元格内, 字号以蓝色粗体显示在 accessoryView
    - 底部说明文字提示切换标签后生效
    Co-authored-by: monkeycode-ai <monkeycode-ai@chaitin.com>

--- Newterm/MTAppDelegate.m
+++ Newterm/MTAppDelegate.m
@@ -2,14 +2,62 @@
 #import "MTController.h"
 #import "MTSettingsController.h"
 
+static UIImage* createTerminalIcon() {
+  UIGraphicsBeginImageContextWithOptions(CGSizeMake(30,30),NO,0);
+  CGContextRef ctx=UIGraphicsGetCurrentContext();
+
+  CGContextSetFillColorWithColor(ctx,[UIColor colorWithWhite:0.15 alpha:1].CGColor);
+  [[UIBezierPath bezierPathWithRoundedRect:CGRectMake(1,1,28,28) cornerRadius:6] fill];
+
+  [[UIColor colorWithRed:0.3 green:0.85 blue:0.3 alpha:1] setFill];
+  [@">_" drawAtPoint:CGPointMake(5,5) withFont:[UIFont fontWithName:@"Courier-Bold" size:18]];
+
+  UIImage* img=UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return img;
+}
+
+static UIImage* createSettingsIcon() {
+  UIGraphicsBeginImageContextWithOptions(CGSizeMake(30,30),NO,0);
+  CGContextRef ctx=UIGraphicsGetCurrentContext();
+
+  CGFloat cx=15,cy=15,outerR=13,innerR=9;
+  CGContextSetFillColorWithColor(ctx,[UIColor colorWithWhite:0.35 alpha:1].CGColor);
+
+  for(int i=0;i<8;i++){
+    CGFloat a=i*M_PI/4-M_PI/2;
+    CGFloat w=3,h=5;
+    CGContextSaveGState(ctx);
+    CGContextTranslateCTM(ctx,cx+cosf(a)*innerR,cy+sinf(a)*innerR);
+    CGContextRotateCTM(ctx,a);
+    CGContextFillRect(ctx,CGRectMake(-w/2,-h/2,w,h));
+    CGContextRestoreGState(ctx);
+  }
+
+  CGContextSetFillColorWithColor(ctx,[UIColor colorWithWhite:0.55 alpha:1].CGColor);
+  CGContextFillEllipseInRect(ctx,CGRectMake(cx-outerR,cy-outerR,outerR*2,outerR*2));
+  CGContextSetFillColorWithColor(ctx,[UIColor colorWithWhite:0.35 alpha:1].CGColor);
+  CGContextFillEllipseInRect(ctx,CGRectMake(cx-innerR,cy-innerR,innerR*2,innerR*2));
+  CGContextSetFillColorWithColor(ctx,[UIColor colorWithWhite:0.55 alpha:1].CGColor);
+  CGContextFillEllipseInRect(ctx,CGRectMake(cx-4,cy-4,8,8));
+
+  UIImage* img=UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return img;
+}
+
 @implementation MTAppDelegate
 -(void)applicationDidFinishLaunching:(UIApplication*)application {
   window=[[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
 
   controller=[[MTController alloc] init];
   controller.title=@"Term";
+  controller.tabBarItem=[[[UITabBarItem alloc] initWithTitle:@"Term"
+    image:createTerminalIcon() tag:0] autorelease];
 
   MTSettingsController* settingsController=[[MTSettingsController alloc] init];
+  settingsController.tabBarItem=[[[UITabBarItem alloc] initWithTitle:@"\u8bbe\u7f6e"
+    image:createSettingsIcon() tag:1] autorelease];
 
   tabBarController=[[UITabBarController alloc] init];
   tabBarController.viewControllers=[NSArray arrayWithObjects:controller,settingsController,nil];
--- Newterm/MTSettingsController.m
+++ Newterm/MTSettingsController.m
@@ -1,6 +1,7 @@
 #import "MTSettingsController.h"
 
-@interface MTSettingsController () {
+@interface MTSettingsController () <UITableViewDataSource,UITableViewDelegate> {
+  UITableView* tableView;
   UISlider* fontSizeSlider;
   UILabel* fontSizeLabel;
 }
@@ -11,78 +12,94 @@
 -(void)loadView {
   [self setTitle:@"\u8bbe\u7f6e"];
 
-  UIView* view = [[UIView alloc] initWithFrame:CGRectZero];
-  view.backgroundColor = [UIColor groupTableViewBackgroundColor];
-  view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-  self.view = view;
+  UIView* view=[[UIView alloc] initWithFrame:CGRectZero];
+  view.autoresizingMask=UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+  self.view=view;
   [view release];
 
-  CGFloat y = 20;
-  CGFloat w = view.bounds.size.width;
+  tableView=[[UITableView alloc] initWithFrame:view.bounds
+    style:UITableViewStyleGrouped];
+  tableView.autoresizingMask=UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+  tableView.dataSource=self;
+  tableView.delegate=self;
+  tableView.backgroundView=nil;
+  tableView.backgroundColor=[UIColor colorWithWhite:0.93 alpha:1];
+  [view addSubview:tableView];
+  [tableView release];
 
-  UILabel* titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, y, w-40, 30)];
-  titleLabel.text = @"\u7ec8\u7aef\u5b57\u4f53\u5927\u5c0f";
-  titleLabel.font = [UIFont boldSystemFontOfSize:16];
-  titleLabel.textColor = [UIColor blackColor];
-  titleLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-  [view addSubview:titleLabel];
-  [titleLabel release];
-  y += 35;
+  NSUserDefaults* defaults=[NSUserDefaults standardUserDefaults];
+  NSString* name=[NSBundle mainBundle].bundleIdentifier;
+  NSDictionary* settings=[defaults persistentDomainForName:name];
+  CGFloat currentSize=[[settings objectForKey:@"fontSize"] respondsToSelector:@selector(doubleValue)]?
+    [[settings objectForKey:@"fontSize"] doubleValue]:10;
+  if(currentSize<=0) currentSize=10;
 
-  NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
-  NSString* name = [NSBundle mainBundle].bundleIdentifier;
-  NSDictionary* settings = [defaults persistentDomainForName:name];
-  CGFloat currentSize = [[settings objectForKey:@"fontSize"] respondsToSelector:@selector(doubleValue)] ?
-    [[settings objectForKey:@"fontSize"] doubleValue] : 10;
-  if(currentSize <= 0) currentSize = 10;
+  fontSizeSlider=[[[UISlider alloc] initWithFrame:CGRectMake(0,0,200,30)] autorelease];
+  fontSizeSlider.minimumValue=6;
+  fontSizeSlider.maximumValue=24;
+  fontSizeSlider.value=currentSize;
+  [fontSizeSlider addTarget:self action:@selector(fontSizeChanged:)
+    forControlEvents:UIControlEventValueChanged];
+}
 
-  fontSizeLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, y, w-40, 25)];
-  fontSizeLabel.text = [NSString stringWithFormat:@"%.0f pt", currentSize];
-  fontSizeLabel.font = [UIFont systemFontOfSize:14];
-  fontSizeLabel.textColor = [UIColor darkGrayColor];
-  fontSizeLabel.textAlignment = NSTextAlignmentCenter;
-  fontSizeLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-  [view addSubview:fontSizeLabel];
-  y += 30;
+-(NSInteger)numberOfSectionsInTableView:(UITableView*)tv {
+  return 1;
+}
 
-  fontSizeSlider = [[UISlider alloc] initWithFrame:CGRectMake(40, y, w-80, 30)];
-  fontSizeSlider.minimumValue = 6;
-  fontSizeSlider.maximumValue = 24;
-  fontSizeSlider.value = currentSize;
-  fontSizeSlider.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-  [fontSizeSlider addTarget:self action:@selector(fontSizeChanged:) forControlEvents:UIControlEventValueChanged];
-  [view addSubview:fontSizeSlider];
-  [fontSizeSlider release];
+-(NSInteger)tableView:(UITableView*)tv numberOfRowsInSection:(NSInteger)section {
+  return 1;
+}
 
-  UILabel* minLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, y, 30, 30)];
-  minLabel.text = @"6";
-  minLabel.font = [UIFont systemFontOfSize:11];
-  minLabel.textColor = [UIColor grayColor];
-  minLabel.textAlignment = NSTextAlignmentCenter;
-  [view addSubview:minLabel];
-  [minLabel release];
+-(NSString*)tableView:(UITableView*)tv titleForHeaderInSection:(NSInteger)section {
+  return @"\u7ec8\u7aef\u5b57\u4f53";
+}
 
-  UILabel* maxLabel = [[UILabel alloc] initWithFrame:CGRectMake(w-40, y, 30, 30)];
-  maxLabel.text = @"24";
-  maxLabel.font = [UIFont systemFontOfSize:11];
-  maxLabel.textColor = [UIColor grayColor];
-  maxLabel.textAlignment = NSTextAlignmentCenter;
-  maxLabel.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin;
-  [view addSubview:maxLabel];
-  [maxLabel release];
+-(NSString*)tableView:(UITableView*)tv titleForFooterInSection:(NSInteger)section {
+  return @"\u8c03\u6574\u7ec8\u7aef\u6587\u672c\u663e\u793a\u5927\u5c0f\uff0c\u5207\u6362\u56de Term \u6807\u7b7e\u540e\u751f\u6548";
+}
+
+-(UITableViewCell*)tableView:(UITableView*)tv cellForRowAtIndexPath:(NSIndexPath*)indexPath {
+  static NSString* ident=@"fontCell";
+  UITableViewCell* cell=[tv dequeueReusableCellWithIdentifier:ident];
+  if(!cell){
+    cell=[[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1
+      reuseIdentifier:ident] autorelease];
+    cell.selectionStyle=UITableViewCellSelectionStyleNone;
+
+    fontSizeLabel=[[UILabel alloc] initWithFrame:CGRectMake(0,0,60,30)];
+    fontSizeLabel.font=[UIFont boldSystemFontOfSize:16];
+    fontSizeLabel.textColor=[UIColor colorWithRed:0.2 green:0.4 blue:0.8 alpha:1];
+    fontSizeLabel.textAlignment=NSTextAlignmentCenter;
+    fontSizeLabel.text=[NSString stringWithFormat:@"%.0f pt",fontSizeSlider.value];
+    cell.accessoryView=fontSizeLabel;
+    [fontSizeLabel release];
+
+    CGRect frame=fontSizeSlider.frame;
+    frame.origin.x=15;
+    frame.origin.y=(44-frame.size.height)/2;
+    frame.size.width=tv.bounds.size.width-30-70;
+    fontSizeSlider.frame=frame;
+    fontSizeSlider.autoresizingMask=UIViewAutoresizingFlexibleWidth;
+    [cell.contentView addSubview:fontSizeSlider];
+  }
+  return cell;
 }
 
 -(void)fontSizeChanged:(UISlider*)slider {
-  CGFloat fontSize = round(slider.value);
-  fontSizeLabel.text = [NSString stringWithFormat:@"%.0f pt", fontSize];
+  CGFloat fontSize=round(slider.value);
+  fontSizeLabel.text=[NSString stringWithFormat:@"%.0f pt",fontSize];
 
-  NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
-  NSString* name = [NSBundle mainBundle].bundleIdentifier;
-  NSMutableDictionary* settings = [[defaults persistentDomainForName:name] mutableCopy];
-  if(!settings) settings = [[NSMutableDictionary alloc] init];
+  NSUserDefaults* defaults=[NSUserDefaults standardUserDefaults];
+  NSString* name=[NSBundle mainBundle].bundleIdentifier;
+  NSMutableDictionary* settings=[[defaults persistentDomainForName:name] mutableCopy];
+  if(!settings) settings=[[NSMutableDictionary alloc] init];
   [settings setObject:[NSNumber numberWithDouble:fontSize] forKey:@"fontSize"];
   [defaults setPersistentDomain:settings forName:name];
   [settings release];
+}
+
+-(void)dealloc {
+  [super dealloc];
 }
 
 @end
