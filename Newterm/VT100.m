5023839f999fe4fd8794632ed7c817e4912228ca
wyxdlz54188 <monkeycode-ai@chaitin.com>
2026-06-20 12:22:38 +0000

    fix: restore LANG env for shell UTF-8 support Co-authored-by: monkeycode-ai <monkeycode-ai@chaitin.com>

--- Newterm/VT100.m
+++ Newterm/VT100.m
@@ -122,7 +122,7 @@ static void screen_line_release(CFAllocatorRef allocator,screen_line_t* line) {
     else if(pid==0){
       if(execve("/usr/bin/login",
        (char*[]){"login","-fp",getlogin(),NULL},
-       (char*[]){"TERM=xterm-256color","HOME=/var/mobile",NULL})==-1)
+       (char*[]){"TERM=xterm-256color","LANG=en_US.UTF-8","HOME=/var/mobile",NULL})==-1)
         raiseException(@"execve(login)");
     }
     else {
